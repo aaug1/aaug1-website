9c910ee3b83760b8e60fd212f42278889a4ca67c
Aidan Aug <aidanaug123@gmail.com>
2022-11-23 00:18:27 -0500

    Back to default of using nextConfig

--- next.config.js
+++ next.config.js
@@ -11,19 +11,4 @@
   assetPath: "/aaug1-website/"
 };
 
-module.exports = {
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: `/_next/static/:path*`,
-          destination: '/aaug1-website/_next/static/:path*'
-         },
-         {
-          source: `/images/:path*`,
-          destination: '/aaug1-website/images/:path*'
-         },
-      ]
-    }
-  },
-}
+module.exports = nextConfig;
